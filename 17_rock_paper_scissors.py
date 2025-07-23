070dc67106334b5413a509170cea86a41783458c
Vishnu G <vishnugkannath@gmail.com>
2025-07-23 13:40:22 +0530

    changed full functional codes and old codes have been hashed for references

--- 17_rock_paper_scissors.py
+++ 17_rock_paper_scissors.py
@@ -28,13 +28,36 @@ scissors = '''
 ---.__(___)
 ✂️
 '''
+import random
 
-choice = int(input("Type 0 for Rock 🪨, 1 for Paper 📃 or 2 for Scissors ✂️.: "))
-if choice == 0:
-    print(rock)
-elif choice == 1:
-    print(paper)
-elif choice ==  2:
-    print(scissors)
-else:
-    print("Invalid Input!")
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("Type 0 for Rock 🪨, 1 for Paper 📃 or 2 for Scissors ✂️.:\n "))
+# if user_choice == 0:
+#     print(rock)
+# elif user_choice == 1:
+#     print(paper)
+# elif user_choice ==  2:
+#     print(scissors)
+# else:
+#     print("Invalid Input!")
+
+if user_choice >= 0 and user_choice <= 2:
+    print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer choice:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice ==  0 and computer_choice == 2:
+    print("You Win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You Lose!")
+elif user_choice > computer_choice:
+    print("You Win!")
+elif computer_choice > user_choice:
+    print("You Lose!")
+elif computer_choice == user_choice:
+    print("It's a draw")
